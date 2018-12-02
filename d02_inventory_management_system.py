cc5505c9367eef0bd379d1b799285ad0b34bf8bc
Pietro Brunetti <gunzapper@gmail.com>
2018-12-02 18:12:45 +0100

    second part solved

--- d02_inventory_management_system.py
+++ d02_inventory_management_system.py
@@ -1,4 +1,5 @@
 from collections import Counter
+from difflib import get_close_matches, ndiff
 
 
 def checksum(box_ids: str) -> int:
@@ -33,8 +34,39 @@ def checksum(box_ids: str) -> int:
     return how_many_two * how_many_three
 
 
+def in_common(box_ids: str) -> str:
+    """
+    Search ids that differ by one letter
+    and returns characters in commons.
+
+    >>> in_common([
+    ... 'abcde',  # differ of 2 to the 5 - too much
+    ... 'fghij',  # differ of 1 to the 4
+    ... 'klmno',
+    ... 'pqrst',
+    ... 'fguij',
+    ... 'axcye',
+    ... 'wvxyz',
+    ... ])
+    'fgij'
+
+    :param box_ids: list of the ids
+    :return: the characters in commons of the sequences that differ by one.
+    """
+    for i, sequence in enumerate(box_ids[:-1]):
+        closed = get_close_matches(sequence, box_ids[i+1:], n=1, cutoff=0.8)
+        if len(closed) == 1:
+            closest = closed[0]
+            in_common = ''.join(
+                [li[2] for li in ndiff(sequence, closest) if li[0] == ' ']
+            )
+            if len(in_common) == len(sequence) - 1:
+                return(in_common)
+
+
 if __name__ == "__main__":
     with open("./input_d02.txt") as handle:
         box_ids = handle.readlines()
 
-    print(checksum(box_ids))
+    print(checksum(box_ids))  # -> 6723
+    print(in_common(box_ids))  # -> prtkqyluiusocwvaezjmhmfgx
