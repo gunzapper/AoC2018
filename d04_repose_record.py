22c8453beeb389b96eff46fa855289326af7bddc
Pietro Brunetti <gunzapper@gmail.com>
2018-12-04 23:36:37 +0100

    starting the day four

---
+++ d04_repose_record.py
@@ -0,0 +1,15 @@
+# 0 read the logs
+
+# 1 sort by chronological order the logs
+# by convert the timestamp in datetime
+# use the datetime for organize a list of tuples
+# [(datetime, descriptions)]
+
+# 2 if description start with 'Guard #' i find the id
+# the bollowing logs are couple 'falls asleep' - 'wake-up'
+# for each couple compute a timedelta.
+# sums the time delta
+# find the guard with this time greater
+
+# 3 for the more asleep guard find the minute
+# when he often falls asleep
