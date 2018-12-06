33ac96290c5e3a92654c1bc15be6ca9961c257b4
Pietro Brunetti <gunzapper@gmail.com>
2018-12-06 17:27:15 +0100

    resolved the day 5 first excercize

---
+++ d05_alchemical_reduction.py
@@ -0,0 +1,49 @@
+import string
+from itertools import chain
+import re
+
+lower_upper = (
+    ''.join(c) for c in zip(string.ascii_lowercase, string.ascii_uppercase)
+)
+upper_lower = (
+    ''.join(c) for c in zip(string.ascii_uppercase, string.ascii_lowercase)
+)
+opposites = chain(upper_lower, lower_upper)
+
+pattern = re.compile("|".join(opposites))
+
+
+def reaction(polymer: str) -> str:
+    """
+    Chain reaction annihilates opposite units.
+
+    >>> reaction("Aa")
+    ''
+    >>> reaction("abBA")
+    ''
+    >>> reaction("abAB")
+    'abAB'
+    >>> reaction("aabAAB")
+    'aabAAB'
+    >>> reaction("dabAcCaCBAcCcaDA")
+    'dabCBAcaDA'
+    """
+    old_polymer = polymer
+    while True:
+        polymer = re.sub(pattern, "", polymer)
+
+        if polymer == old_polymer:
+            return polymer
+
+        old_polymer = polymer
+
+
+with open("./input_d05.txt") as handle:
+    polymer = handle.read()
+
+# remove the EOL
+polymer = polymer[:-1]
+
+print(len(polymer))
+polymer = reaction(polymer)
+print(len(polymer))
